1f5da4658accb0cbb1bf91f6d02ff2a1ef5c2aa1
lwchen <j630935506@gmail.com>
2023-04-07 12:21:40 +0800

    align ui and fix slider

--- extract_video_ui.py
+++ extract_video_ui.py
@@ -1,22 +1,96 @@
 import cv2
 import os
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk
+# Import additional modules
+import numpy as np
+
+
+# Global variables
+video_path = ""
+frame_number = 0
+regions = []
 
 # Set up the UI
 root = Tk()
 root.title("Video Region Extractor")
-root.geometry("400x400")
+root.geometry("800x600")
+
+
+# Custom Canvas for handling mouse events and displaying preview
+class PreviewCanvas(Canvas):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.bind("<Button-1>", self.mouse_callback)
+        self.bind("<B1-Motion>", self.mouse_callback)
+        self.bind("<ButtonRelease-1>", self.mouse_callback)
+
+    def mouse_callback(self, event):
+        x, y = int(event.x / self.ratio), int(event.y / self.ratio)
+        event_type = None
+        if event.type == "4":  # Button Press
+            event_type = cv2.EVENT_LBUTTONDOWN
+        elif event.type == "5":  # Button Release
+            event_type = cv2.EVENT_LBUTTONUP
+        elif event.type == "6":  # Mouse Move
+            event_type = cv2.EVENT_MOUSEMOVE
+        mouse_callback(event_type, x, y, None, None)
+
+# Set up the preview window
+preview_canvas = PreviewCanvas(root, width=500, height=500)
+preview_canvas.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
+
+
 
 # Select the video file
 def select_file():
-    file_path = filedialog.askopenfilename()
+    global video_path, frame_number
+    video_path = filedialog.askopenfilename()
     file_path_entry.delete(0, END)
-    file_path_entry.insert(0, file_path)
+    file_path_entry.insert(0, video_path)
+    update_frame()
+    cap = cv2.VideoCapture(video_path)
+    frame_number = 0
+    slider.configure(to=int(cap.get(cv2.CAP_PROP_FRAME_COUNT)-1))
+    slider.set(0)
+    cap.release()
+
+# Update the preview frame
+def update_frame():
+    global frame_number
+    cap = cv2.VideoCapture(video_path)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+    ret, frame = cap.read()
+    if ret == True:
+        # Draw selected regions on frame
+        for (x, y, w, h) in regions:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # Resize and display the frame
+        height, width, channels = frame.shape
+        ratio = 1
+        if width > height:
+            ratio = 500 / width
+        else:
+            ratio = 500 / height
+        resized_frame = cv2.resize(frame, (int(width*ratio), int(height*ratio)))
+        preview_canvas.ratio = ratio
+        cv2image = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGBA)
+        img = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image=img)
+        preview_canvas.imgtk = imgtk
+        preview_canvas.create_image(0, 0, image=imgtk, anchor=NW)
+    cap.release()
+
+# Handle slider movement
+def slider_moved(value):
+    global frame_number
+    frame_number = int(value)
+    update_frame()
+
 
 # Create the output directories
 def create_directories():
-    video_path = file_path_entry.get()
     output_path = os.path.splitext(video_path)[0]
     if not os.path.exists(output_path):
         os.makedirs(output_path)
@@ -26,16 +100,71 @@ def create_directories():
             os.makedirs(region_path)
 
 # Extract the selected regions from the video and save as separate video files
-def extract_regions():
-    video_path = file_path_entry.get()
+# Update the preview frame
+def update_frame():
+    global frame_number
     cap = cv2.VideoCapture(video_path)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+    ret, frame = cap.read()
+    if ret == True:
+        # Draw selected regions on frame
+        for (x, y, w, h) in regions:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # Resize and display the frame
+        height, width, channels = frame.shape
+        ratio = 1
+        if width > height:
+            ratio = 500 / width
+        else:
+            ratio = 500 / height
+        resized_frame = cv2.resize(frame, (int(width*ratio), int(height*ratio)))
+        cv2image = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGBA)
+        img = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image=img)
+        preview_label.imgtk = imgtk
+        preview_label.configure(image=imgtk)
+    cap.release()
+
+# Handle mouse events on preview window
+def mouse_callback(event, x, y, flags, param):
+    global regions
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # Start dragging a new region
+        regions.append((x, y, 0, 0))
+    elif event == cv2.EVENT_MOUSEMOVE:
+        # Update the last region being dragged
+        if len(regions) > 0:
+            x1, y1, _, _ = regions[-1]
+            w = x - x1
+            h = y - y1
+            regions[-1] = (x1, y1, w, h)
+            update_frame()
+    elif event == cv2.EVENT_LBUTTONUP:
+        # Stop dragging the last region
+        if len(regions) > 0:
+            x1, y1, w, h = regions[-1]
+            if w < 0:
+                x1 += w
+                w = abs(w)
+            if h < 0:
+                y1 += h
+                h = abs(h)
+            regions[-1] = (x1, y1, w, h)
+            update_frame()
+
+# Extract the selected regions from the video and save as separate video files
+def extract_regions():
+    # Create video capture object and get video properties
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     # Create video writer objects for all selected regions
     output_path = os.path.splitext(video_path)[0]
     writers = []
     for j, (x, y, w, h) in enumerate(regions):
         region_path = os.path.join(output_path, "region_" + str(j), "output.mp4")
-        fps = cap.get(cv2.CAP_PROP_FPS)
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         writer = cv2.VideoWriter(region_path, fourcc, fps, (w, h))
         writers.append(writer)
@@ -54,34 +183,43 @@ def extract_regions():
     for writer in writers:
         writer.release()
 
+    messagebox.showinfo("Extraction Complete", "Regions extracted successfully!")
+
 
 
 # Set up the file path entry
 file_path_label = Label(root, text="Video File:")
-file_path_label.pack()
+file_path_label.grid(row=0, column=0, padx=10, pady=10)
 file_path_entry = Entry(root, width=50)
-file_path_entry.pack()
+file_path_entry.grid(row=0, column=1, padx=10, pady=10)
 select_file_button = Button(root, text="Select", command=select_file)
-select_file_button.pack()
+select_file_button.grid(row=0, column=2, padx=10, pady=10)
+
+# Set up the preview window
+preview_label = Label(root)
+preview_label.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
+cv2.startWindowThread()
+cv2.setMouseCallback("Preview", mouse_callback)
+
+# Set up the slider
+slider_frame = Frame(root)
+slider_frame.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
+slider_label = Label(slider_frame, text="Frame Number:")
+slider_label.grid(row=0, column=0, sticky=W)
+slider_number_label = Label(slider_frame)
+slider_number_label.grid(row=0, column=1, padx=10, sticky=W)
+slider = Scale(slider_frame, from_=0, to=0, command=slider_moved, orient=HORIZONTAL, length=600, resolution=1, troughcolor="white", takefocus=1)
+slider.grid(row=0, column=2, sticky=W)
+
 
 # Set up the region selection buttons
-regions = []
-def select_region():
-    global regions
-    cap = cv2.VideoCapture(file_path_entry.get())
-    ret, frame = cap.read()
-    cv2.namedWindow("Select Region")
-    cv2.imshow("Select Region", frame)
-    rect = cv2.selectROI("Select Region", frame, fromCenter=False, showCrosshair=True)
-    regions.append(rect)
-    cv2.destroyWindow("Select Region")
-    cap.release()
-
-select_region_button = Button(root, text="Select Region", command=select_region)
-select_region_button.pack()
+select_region_button = Button(root, text="Select Region", command=lambda:[create_directories(), extract_regions()])
+select_region_button.grid(row=3, column=0, padx=10, pady=10)
+clear_regions_button = Button(root, text="Clear Regions", command=lambda:[regions.clear(), update_frame()])
+clear_regions_button.grid(row=3, column=1, padx=10, pady=10)
 
 # Set up the extraction button
 extract_button = Button(root, text="Extract Regions", command=lambda:[create_directories(), extract_regions()])
-extract_button.pack()
+extract_button.grid(row=3, column=2, padx=10, pady=10)
 
 root.mainloop()
--- yolos_demo.py
+++ yolos_demo.py
@@ -11,60 +11,71 @@ def box_cxcywh_to_xyxy(x):
          (x_c + 0.5 * w), (y_c + 0.5 * h)]
     return torch.stack(b, dim=-1)
 
-# Read the video file
-cap = cv2.VideoCapture('VID_20230131_103150_1_trunc.mp4')
+def build_model():
+    model_id = 'hustvl/yolos-small'
+    feature_extractor = YolosFeatureExtractor.from_pretrained(model_id)
+    model = YolosForObjectDetection.from_pretrained(model_id)
+    return feature_extractor, model
 
-# Extract the first frame
-_, image = cap.read()
-# Convert the image from BGR to RGB
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-image = Image.fromarray(image)
-# image_pil = Image.open('./dog-puppy-on-garden-royalty-free-image-1586966191.jpeg')
+# add iou_threshold and score_threshold to run_detector as arguments
+def run_detector(image, feature_extractor, model, iou_threshold=0.5, score_threshold=0.5):
+    inputs = feature_extractor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
+    bboxes = outputs.pred_boxes
 
-model_id = 'hustvl/yolos-small'
-feature_extractor = YolosFeatureExtractor.from_pretrained(model_id)
-model = YolosForObjectDetection.from_pretrained(model_id)
-# device = 'cuda' if torch.cuda.is_available() else 'cpu'
-device = 'cpu'
-model = model.to(device)
+    # decode the boxes in YOLO format
+    bboxes = box_cxcywh_to_xyxy(bboxes)[0]
+    # convert scores to probabilities
+    prob = logits.softmax(-1)[0]
+    scores, labels = prob[..., :-1].max(-1)
 
-inputs = feature_extractor(images=image, return_tensors="pt").to(device)
-outputs = model(**inputs)
-# img_shape = inputs['pixel_values'].shape[-2:]
+    # perform NMS to filter out overlapping boxes
+    box_idx = ops.nms(bboxes, scores, iou_threshold=iou_threshold)
+    box_idx = box_idx[scores[box_idx] > score_threshold]
+    nms_boxes = bboxes[box_idx]
+    nms_scores = scores[box_idx]
+    labels = labels[box_idx]
+    return nms_boxes, nms_scores, labels
 
-# model predicts bounding boxes and corresponding COCO classes
-logits = outputs.logits
-bboxes = outputs.pred_boxes
+def detect_laptop(image):
+    feature_extractor, model = build_model()
+    nms_boxes, nms_scores, labels = run_detector(image, feature_extractor, model, score_threshold=0.5)
 
-# decode the boxes in YOLO format
-bboxes = box_cxcywh_to_xyxy(bboxes)[0]
-# convert scores to probabilities
-scores = logits.softmax(-1)[0]
+    # Detect the position of laptop
+    laptop_class = model.config.label2id['laptop']
 
-# Detect the position of laptop
-laptop_class = model.config.label2id['laptop']
-scores = scores[:,laptop_class]
+    is_target = labels == laptop_class
+    nms_boxes = nms_boxes[is_target]
+    nms_scores = nms_scores[is_target]
+    labels = labels[is_target]
+    labels = [model.config.id2label[int(label)] for label in labels]
+    # Unnormalize the box coordinates and plot the boxes and scores on the image
+    nms_boxes = nms_boxes * torch.tensor([image.width, image.height, image.width, image.height])
+    return nms_boxes, nms_scores, labels
 
-# perform NMS to filter out overlapping boxes
-box_idx = ops.nms(bboxes, scores, iou_threshold=0.5)
-box_idx = box_idx[scores[box_idx] > 0.3]
-nms_boxes = bboxes[box_idx]
-nms_scores = scores[box_idx]
 
-# Unnormalize the box coordinates and plot the boxes and scores on the image
-draw = ImageDraw.Draw(image)
-for box, score in zip(nms_boxes, nms_scores):
-    # Unnormalize the box coordinates
-    x1, y1, x2, y2 = box
-    w, h = x2 - x1, y2 - y1
-    x_center, y_center = x1 + w / 2, y1 + h / 2
-    x1, y1, x2, y2 = x1 * image.width, y1 * image.height, x2 * image.width, y2 * image.height
+if __name__ == '__main__':
+    # Read the video file
+    cap = cv2.VideoCapture('VID_20230131_103150_1_trunc.mp4')
 
-    # Draw the box and score on the image
-    draw.rectangle((x1, y1, x2, y2), outline ="red", width=2)
-    try:
-        text = '%s: %.2f'%(model.config.id2label[laptop_class], float(score))
-    except KeyError:
-        text = '%s: %.2f'%('Unknown', float(score))
-    draw.text((x_center - 10, y_center - 10), text, fill="red")
-image.show()
+    # Extract the first frame
+    _, image = cap.read()
+
+    # Convert the image from BGR to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(image)
+    nms_boxes, nms_scores, labels = detect_laptop(image)
+
+    draw = ImageDraw.Draw(image)
+    for box, score, label in zip(nms_boxes, nms_scores, labels):
+        # Unnormalize the box coordinates
+        x1, y1, x2, y2 = box
+        w, h = x2 - x1, y2 - y1
+        x_center, y_center = x1 + w / 2, y1 + h / 2
+
+        # Draw the box and score on the image
+        draw.rectangle((x1, y1, x2, y2), outline ="red", width=2)
+        text = '%s: %.2f'%(label, float(score))
+        draw.text((x_center - 10, y_center - 10), text, fill="red")
+    image.show()
